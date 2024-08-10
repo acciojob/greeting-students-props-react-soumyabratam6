cd764e66f4ee4ff2108a157089e2d2bb2748351a
soumyabrata <soumyabratam6@gmail.com>
2024-08-10 21:00:15 +0530

    all file added

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,11 @@
 // write code for Welcome component here
+import React from 'react'
+
+export const Welcome = ({name}) => {
+  return (
+    <div>
+        <h1>Hey {name}!</h1>
+        <p><h2>Welcome to School.</h2></p>
+    </div>
+  )
+}
